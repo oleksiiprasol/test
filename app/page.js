752e264d2bbe1b5efe258c8562115c086d372ac6
oleksiiprasol <oleksiiprasol@gmail.com>
2023-11-14 21:08:29 +0200

    test fonts

---
+++ app/page.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="box">
+      <h1 className="h1">Привіт це перевірка шрифтів для YOGATODAYua</h1>
+      <h2 className="h2">Привіт це перевірка шрифтів для YOGATODAYua</h2>
+      <h3 className="h3">Привіт це перевірка шрифтів для YOGATODAYua</h3>
+      <h4 className="h4">Привіт це перевірка шрифтів для YOGATODAYua</h4>
+      <h5 className="h5">Привіт це перевірка шрифтів для YOGATODAYua</h5>
+      <h6 className="h6">Привіт це перевірка шрифтів для YOGATODAYua</h6>
+      <p className="p1">Привіт це перевірка шрифтів для YOGATODAYua</p>
+      <p className="p2">Привіт це перевірка шрифтів для YOGATODAYua</p>
+      <p className="p3">Привіт це перевірка шрифтів для YOGATODAYua</p>
+      <p className="p4">Привіт це перевірка шрифтів для YOGATODAYua</p>
+    </div>
+  );
+}
