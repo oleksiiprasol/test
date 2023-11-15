9d40cc7ddc3d80501776018b108e7506edaac88c
oleksiiprasol <oleksiiprasol@gmail.com>
2023-11-15 11:14:51 +0200

    test fonts

--- app/page.js
+++ app/page.js
@@ -4,20 +4,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className="box">
-      <p className="p1">Logo font:</p>
+      <p>Logo font:</p>
       <h1 className="logo">YOGATODAYua</h1>
-      <p className="p1">Similar "Excentra Pro" font:</p>
+      <p>Similar "Excentra Pro" font:</p>
       <h1 className="similar_logo_font_excentra_pro">YOGATODAYua</h1>
-      <h1 className="h1">Привіт це перевірка шрифтів для YOGATODAYua</h1>
-      <h2 className="h2">Привіт це перевірка шрифтів для YOGATODAYua</h2>
-      <h3 className="h3">Привіт це перевірка шрифтів для YOGATODAYua</h3>
-      <h4 className="h4">Привіт це перевірка шрифтів для YOGATODAYua</h4>
-      <h5 className="h5">Привіт це перевірка шрифтів для YOGATODAYua</h5>
-      <h6 className="h6">Привіт це перевірка шрифтів для YOGATODAYua</h6>
-      <p className="p1">Привіт це перевірка шрифтів для YOGATODAYua</p>
-      <p className="p2">Привіт це перевірка шрифтів для YOGATODAYua</p>
-      <p className="p3">Привіт це перевірка шрифтів для YOGATODAYua</p>
-      <p className="p4">Привіт це перевірка шрифтів для YOGATODAYua</p>
     </div>
   );
 }
